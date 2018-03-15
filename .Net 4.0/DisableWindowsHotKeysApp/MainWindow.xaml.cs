d18533eedb46c0dc19618cd7f24af6b900fd8a25
Novikov Egor <novikov.ea@mail.ru>
2018-03-15 11:21:53 +0300

    DisableWindowsHotKeysApp added

---
+++ .Net 4.0/DisableWindowsHotKeysApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace DisableWindowsHotKeysApp
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PreviewKeyDown += MainWindow_PreviewKeyDown;
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine("Modifiers={0}; Key={1}; KeyStates={2}; SystemKey = {3}", Keyboard.Modifiers, e.Key, e.KeyStates, e.SystemKey);
+
+            if (Keyboard.Modifiers == ModifierKeys.Alt && e.SystemKey == Key.Space)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnKeyDown(e);
+            }
+        }
+    }
+}
